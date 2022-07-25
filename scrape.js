6614773476f66a0ae17f4c5722e957ce091d170d
Ayush Sharma <100598965+iamay@users.noreply.github.com>
2022-07-25 18:03:35 +0530

    Add files via upload

---
+++ scrape.js
@@ -0,0 +1,50 @@
+//Packages
+const axios = require("axios");
+const cheerio = require("cheerio");
+require("dotenv").config();
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+const client = require("twilio")(accountSid, authToken);
+
+const url =
+"https://www.amazon.in/Apple-iPhone-13-128GB-Starlight/dp/B09G9D8KRQ/ref=sr_1_3?keywords=iphone+13&qid=1658657905&sprefix=iphone+%2Caps%2C279&sr=8-3";
+
+const product = { name: "", price: "", link: "" };
+
+//Set interval
+const handle = setInterval(scrape, 20000);
+
+async function scrape() {
+  //Fetch the data
+  const { data } = await axios.get(url);
+  //Load up the html
+  const $ = cheerio.load(data);
+  const item = $("div#dp-container");
+  //Extract the data that we need
+  product.name = $(item).find("h1 span#productTitle").text();
+  product.link = url;
+  const price = $(item)
+    .find("span .a-price-whole")
+    .first()
+    .text()
+    .replace(/[,.]/g, "");
+  const priceNum = parseInt(price);
+  product.price = priceNum;
+  console.log(product);
+  //Send an SMS
+  if (priceNum < 70000) {
+    client.messages
+      .create({
+        body: `The price of ${product.name} went below ${price}. Purchase it at ${product.link}`,
+        messagingServiceSid: 'MG6869aa64a0960b9473de569d50ef2e17',      
+        to: "+916006296494",
+      })
+      .then((message) => {
+        console.log(message);
+        clearInterval(handle);
+      });
+  }
+}
+
+scrape(); //Run the scrape function
+  
